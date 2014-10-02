0908fd98f23674304dec9b28f9fd0f667e558c78
shimonenator <shimonenator@gmail.com>
2014-10-02 15:31:07 -0400

    Finished initial version

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 /* Copyright (c) 2014 Shimon Schwartz, MIT License */
-/*jslint node: true */
 
 var AbstractLevelDOWN = require('abstract-leveldown').AbstractLevelDOWN,
 	couchbase = require('couchbase');
@@ -8,7 +7,7 @@ function CouchbaseDOWN(location) {
 	if (!(this instanceof CouchbaseDOWN)) {
 		return new CouchbaseDOWN(location);
 	}
-	
+
 	AbstractLevelDOWN.call(this, location);
 }
 
@@ -20,10 +19,16 @@ CouchbaseDOWN.prototype = Object.create(AbstractLevelDOWN.prototype, {
 		writable: true
 	},
 
-	_open: function (options, callback) {
-		var that = this, manager;
+	_cluster:	null,
+	_manager:	null,
+	_bucket:	null,
 
-		this._cluster || this._cluster = new couchbase.Cluster(location);
+	_open: function (options, callback) {
+		var self = this, manager;
+
+		if (!this._cluster) {
+			this._cluster = new couchbase.Cluster(location);
+		}
 
 		if (options.createIfMissing || options.errorIfExists) {
 			if (!options.adminUser) {
@@ -37,10 +42,12 @@ CouchbaseDOWN.prototype = Object.create(AbstractLevelDOWN.prototype, {
 			manager = this._cluster.manager(options.adminUser, options.adminPass);
 
 			manager.listBuckets(function (err, bucketInfo) {
-				var exists;
-
 				if (err) {
-					return callback(err);
+					process.nextTick(function () {
+						callback(err);
+					});
+
+					return;
 				}
 
 				// TODO
@@ -58,239 +65,54 @@ CouchbaseDOWN.prototype = Object.create(AbstractLevelDOWN.prototype, {
 					}
 				}
 
-				that._bucket = that._cluster.openBucket(options.bucket || null, options.pass || null, callback);
+				self._openBucket(options.bucket, options.pass, callback);
 			});
 		} else {
-			this._bucket = that._cluster.openBucket(options.bucket || null, options.pass || null, callback);	
+			self._openBucket(options.bucket, options.pass, callback);
 		}
 	},
+
+	_openBucket: function (bucket, password, callback) {
+		var self = this;
+
+		process.nextTick(function () {
+			self._bucket = self._cluster.openBucket(bucket || null, password || null, callback);
+		});
+	}
+
 	_close: function (callback) {
-		
+		this._bucket.disconnect();
+
+		process.nextTick(callback)
+	},
+
+	_get: function (key, options, callback) {
+		if (!options.couchbase) {
+			options.couchbase = {};
+		}
+
+		this._bucket.get(key, options.couchbase, callback);
+	},
+
+	_put: function (key, value, options, callback) {
+		if (!options.couchbase) {
+			options.couchbase = {};
+		}
+
+		this._bucket.upsert(key, value, options.couchbase, callback);
+	},
+
+	_del: function (key, options, callback) {
+		if (!options.couchbase) {
+			options.couchbase = {};
+		}
+
+		this._bucket.remove(key, options.couchbase, callback);
+	},
+
+	_batch: function (array, options, callback) {
+		throw new Error('batch() not implemented');
 	}
 });
 
-
-
-
-
-
-
-
-
-var xtend                = require('xtend')
-  , AbstractIterator     = require('./abstract-iterator')
-  , AbstractChainedBatch = require('./abstract-chained-batch');
-
-
-AbstractLevelDOWN.prototype.close = function (callback) {
-  if (typeof callback != 'function')
-    throw new Error('close() requires a callback argument')
-
-  if (typeof this._close == 'function')
-    return this._close(callback)
-
-  process.nextTick(callback)
-}
-
-AbstractLevelDOWN.prototype.get = function (key, options, callback) {
-  var err
-
-  if (typeof options == 'function')
-    callback = options
-
-  if (typeof callback != 'function')
-    throw new Error('get() requires a callback argument')
-
-  if (err = this._checkKey(key, 'key', this._isBuffer))
-    return callback(err)
-
-  if (!this._isBuffer(key))
-    key = String(key)
-
-  if (typeof options != 'object')
-    options = {}
-
-  options.asBuffer = options.asBuffer != false
-
-  if (typeof this._get == 'function')
-    return this._get(key, options, callback)
-
-  process.nextTick(function () { callback(new Error('NotFound')) })
-}
-
-AbstractLevelDOWN.prototype.put = function (key, value, options, callback) {
-  var err
-
-  if (typeof options == 'function')
-    callback = options
-
-  if (typeof callback != 'function')
-    throw new Error('put() requires a callback argument')
-
-  if (err = this._checkKey(key, 'key', this._isBuffer))
-    return callback(err)
-
-  if (!this._isBuffer(key))
-    key = String(key)
-
-  // coerce value to string in node, don't touch it in browser
-  // (indexeddb can store any JS type)
-  if (value != null && !this._isBuffer(value) && !process.browser)
-    value = String(value)
-
-  if (typeof options != 'object')
-    options = {}
-
-  if (typeof this._put == 'function')
-    return this._put(key, value, options, callback)
-
-  process.nextTick(callback)
-}
-
-AbstractLevelDOWN.prototype.del = function (key, options, callback) {
-  var err
-
-  if (typeof options == 'function')
-    callback = options
-
-  if (typeof callback != 'function')
-    throw new Error('del() requires a callback argument')
-
-  if (err = this._checkKey(key, 'key', this._isBuffer))
-    return callback(err)
-
-  if (!this._isBuffer(key))
-    key = String(key)
-
-  if (typeof options != 'object')
-    options = {}
-
-  if (typeof this._del == 'function')
-    return this._del(key, options, callback)
-
-  process.nextTick(callback)
-}
-
-AbstractLevelDOWN.prototype.batch = function (array, options, callback) {
-  if (!arguments.length)
-    return this._chainedBatch()
-
-  if (typeof options == 'function')
-    callback = options
-
-  if (typeof array == 'function')
-    callback = array
-
-  if (typeof callback != 'function')
-    throw new Error('batch(array) requires a callback argument')
-
-  if (!Array.isArray(array))
-    return callback(new Error('batch(array) requires an array argument'))
-
-  if (!options || typeof options != 'object')
-    options = {}
-
-  var i = 0
-    , l = array.length
-    , e
-    , err
-
-  for (; i < l; i++) {
-    e = array[i]
-    if (typeof e != 'object')
-      continue
-
-    if (err = this._checkKey(e.type, 'type', this._isBuffer))
-      return callback(err)
-
-    if (err = this._checkKey(e.key, 'key', this._isBuffer))
-      return callback(err)
-  }
-
-  if (typeof this._batch == 'function')
-    return this._batch(array, options, callback)
-
-  process.nextTick(callback)
-}
-
-//TODO: remove from here, not a necessary primitive
-AbstractLevelDOWN.prototype.approximateSize = function (start, end, callback) {
-  if (   start == null
-      || end == null
-      || typeof start == 'function'
-      || typeof end == 'function') {
-    throw new Error('approximateSize() requires valid `start`, `end` and `callback` arguments')
-  }
-
-  if (typeof callback != 'function')
-    throw new Error('approximateSize() requires a callback argument')
-
-  if (!this._isBuffer(start))
-    start = String(start)
-
-  if (!this._isBuffer(end))
-    end = String(end)
-
-  if (typeof this._approximateSize == 'function')
-    return this._approximateSize(start, end, callback)
-
-  process.nextTick(function () {
-    callback(null, 0)
-  })
-}
-
-AbstractLevelDOWN.prototype._setupIteratorOptions = function (options) {
-  var self = this
-
-  options = xtend(options)
-
-  ;[ 'start', 'end', 'gt', 'gte', 'lt', 'lte' ].forEach(function (o) {
-    if (options[o] && self._isBuffer(options[o]) && options[o].length === 0)
-      delete options[o]
-  })
-
-  options.reverse = !!options.reverse
-  options.keys = options.keys != false
-  options.values = options.values != false
-  options.limit = 'limit' in options ? options.limit : -1
-  options.keyAsBuffer = options.keyAsBuffer != false
-  options.valueAsBuffer = options.valueAsBuffer != false
-
-  return options
-}
-
-AbstractLevelDOWN.prototype.iterator = function (options) {
-  if (typeof options != 'object')
-    options = {}
-
-  options = this._setupIteratorOptions(options)
-
-  if (typeof this._iterator == 'function')
-    return this._iterator(options)
-
-  return new AbstractIterator(this)
-}
-
-AbstractLevelDOWN.prototype._chainedBatch = function () {
-  return new AbstractChainedBatch(this)
-}
-
-AbstractLevelDOWN.prototype._isBuffer = function (obj) {
-  return Buffer.isBuffer(obj)
-}
-
-AbstractLevelDOWN.prototype._checkKey = function (obj, type) {
-
-  if (obj === null || obj === undefined)
-    return new Error(type + ' cannot be `null` or `undefined`')
-
-  if (this._isBuffer(obj)) {
-    if (obj.length === 0)
-      return new Error(type + ' cannot be an empty Buffer')
-  } else if (String(obj) === '')
-    return new Error(type + ' cannot be an empty String')
-}
-
-module.exports.AbstractLevelDOWN    = AbstractLevelDOWN
-module.exports.AbstractIterator     = AbstractIterator
-module.exports.AbstractChainedBatch = AbstractChainedBatch
+module.exports.CouchbaseDOWN = CouchbaseDOWN;
